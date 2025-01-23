a438cd125bb3db03c2fc406197c33d7a638e7987
Douglas Marcelo Monquero <douglas.monquero@gmail.com>
2025-01-23 20:11:59 -0300

    final app1

--- app1/test/index.js
+++ app1/test/index.js
@@ -2,7 +2,11 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const somarNumeros = (num1, num2) => num1 + num2;
+const somarNumeros = (num1, num2) =>{
+    if(typeof num1 === "number" && typeof num2 === "number"){
+        return num1 + num2
+    }else return undefined
+} 
 
 
 describe('Somas', ()=>{
@@ -15,8 +19,17 @@ describe('Somas', ()=>{
 
 describe('Somas', ()=>{
     it('Soma de dois numeros - -2 e 3', (done)=>{
-        const resultado = somarNumeros(2,3);
-        expect(resultado).be.equal(5);
+        const resultado = somarNumeros(-2,3);
+        expect(resultado).be.equal(1);
         done();
+    } )
+})
+
+describe('Somas', ()=>{
+    it('Soma de dois numeros - "teste" e 3', (done)=>{
+        const resultado = somarNumeros("teste",3);
+        expect(resultado).be.equal(undefined);
+        done();
+        console.log(resultado)
     } )
 })
