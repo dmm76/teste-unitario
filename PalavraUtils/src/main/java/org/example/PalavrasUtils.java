dd4dc25a8dee5586e6c05185bec17abb38343462
Douglas Marcelo Monquero <douglas.monquero@gmail.com>
2025-01-23 16:25:15 -0300

    teste com numeros

--- PalavraUtils/src/main/java/org/example/PalavrasUtils.java
+++ PalavraUtils/src/main/java/org/example/PalavrasUtils.java
@@ -2,6 +2,10 @@ package org.example;
 
 public class PalavrasUtils {
     public static int countPalavras(String texto) {
+        if (texto == null || texto.trim().isEmpty()) {
+            return 0; // Retorna 0 para strings vazias ou nulas
+        }
         return texto.trim().split("\\s+").length;
     }
 }
+
--- PalavraUtils/src/test/java/org/example/PalavrasUtilsTeste.java
+++ PalavraUtils/src/test/java/org/example/PalavrasUtilsTeste.java
@@ -4,11 +4,36 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class PalavrasUtilsTeste {
+
     @Test
     public void testContarPalavrasTextoNenhumPalavraSucesso() {
+        String texto = "";
+        int palavras = PalavrasUtils.countPalavras(texto);
+        Assert.assertEquals(0, palavras);
+        System.out.println(palavras);
+    }
+    @Test
+    public void testContarPalavrasTextoPalavraSucesso() {
         String texto = "Ola, mundo maravilhoso";
         int palavras = PalavrasUtils.countPalavras(texto);
         Assert.assertEquals(3, palavras);
         System.out.println(palavras);
     }
+
+    @Test
+    public void testContarPalavrasTextoEspacosPalavraSucesso() {
+        String texto = "Ola, mundo    maravilhoso";
+        int palavras = PalavrasUtils.countPalavras(texto);
+        Assert.assertEquals(3, palavras);
+        System.out.println(palavras);
+    }
+
+    @Test
+    public void testContarPalavrasNumerosPalavraSucesso() {
+        String texto = "1, 2, 3, 4, 5, 6, 7, 8, 9  10";
+        int palavras = PalavrasUtils.countPalavras(texto);
+        Assert.assertEquals(10, palavras);
+        System.out.println(palavras);
+    }
+
 }
